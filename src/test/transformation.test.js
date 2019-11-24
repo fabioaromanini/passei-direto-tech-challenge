4aaf6fdef3eee7b538d766ebbcfd61e07cb4cea3
Fabio Romanini <fabioaromanini@gmail.com>
2019-11-24 15:17:56 -0200

    adds test suite for transformationService.combineByKey

--- src/test/transformation.test.js
+++ src/test/transformation.test.js
@@ -1,1 +1,16 @@
 const transformationService = require('../service/transformation');
+
+const input = require('./data/inputs/transformationService.json');
+const combinedCourse = require('./data/outputs/combinedCourse.json');
+
+describe('Single Field Transformations', () => {
+  test('Combine By Key', () => {
+    const { students: mainEntity, courses: joinEntity } = input;
+
+    const result = transformationService.combineByKey(mainEntity, 'CourseId', joinEntity, 'Id', {
+      Name: 'CourseName',
+    });
+
+    expect(result).toEqual(combinedCourse);
+  });
+});
