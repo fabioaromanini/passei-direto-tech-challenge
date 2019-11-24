91f6211a0a5f1dcce4d4fd5c72e6712af0480634
Fabio Romanini <fabioaromanini@gmail.com>
2019-11-24 15:18:21 -0200

    implements transformationService.combineByKey

--- src/service/transformation.js
+++ src/service/transformation.js
@@ -1,5 +1,30 @@
-function flattenCollection() {
-  return {};
+function indexJoinEntityByKey(joinEntity, joinEntityKey) {
+  const joinEntityByKey = {};
+  joinEntity.forEach(entry => {
+    const key = entry[joinEntityKey];
+    joinEntityByKey[key] = entry;
+  });
+
+  return joinEntityByKey;
 }
 
-exports.flattenCollection = flattenCollection;
+function combineByKey(mainEntity, mainEntityKey, joinEntity, joinEntityKey, fieldNamesMapping) {
+  const joinEntityByKey = indexJoinEntityByKey(joinEntity, joinEntityKey);
+
+  return mainEntity.map(entry => {
+    const key = entry[mainEntityKey];
+    const joinEntry = joinEntityByKey[key];
+
+    const result = { ...entry };
+    Object.keys(joinEntry).forEach(originalField => {
+      const newFieldName = fieldNamesMapping[originalField];
+      if (newFieldName) {
+        result[newFieldName] = joinEntry[originalField];
+      }
+    });
+
+    return result;
+  });
+}
+
+exports.combineByKey = combineByKey;
