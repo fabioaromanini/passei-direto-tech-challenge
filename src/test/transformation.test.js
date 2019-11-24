446dba9cf5033919cd0447d2a268df78b4187768
Fabio Romanini <fabioaromanini@gmail.com>
2019-11-24 15:46:57 -0200

    adds test suite for multiple combine

--- src/test/transformation.test.js
+++ src/test/transformation.test.js
@@ -2,6 +2,7 @@ const transformationService = require('../service/transformation');
 
 const input = require('./data/inputs/transformationService.json');
 const combinedCourse = require('./data/outputs/combinedCourse.json');
+const combinedSubscriptions = require('./data/outputs/combinedSubscriptions.json');
 
 describe('Single Field Transformations', () => {
   test('Combine By Key', () => {
@@ -10,6 +11,23 @@ describe('Single Field Transformations', () => {
     const result = transformationService.combineByKey(mainEntity, 'CourseId', joinEntity, 'Id', {
       Name: 'CourseName',
     });
+
+    expect(result).toEqual(combinedCourse);
+  });
+
+  test('Combine Multiple By Key', () => {
+    const { students: mainEntity, courses: joinEntity } = input;
+
+    const result = transformationService.combineMultipleByKey(
+      mainEntity,
+      'Id',
+      joinEntity,
+      'StudentId',
+      {
+        PaymentDate: 'Date',
+        PlanType: 'Type',
+      }
+    );
 
     expect(result).toEqual(combinedCourse);
   });
