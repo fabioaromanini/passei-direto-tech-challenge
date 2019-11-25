ff3f553e7a8506ad5b58e815ca3174cf8c16380a
Fabio Romanini <fabioaromanini@gmail.com>
2019-11-25 09:53:27 -0200

    adds test suite for join without a correspondent joinEntry

--- src/test/transformation.test.js
+++ src/test/transformation.test.js
@@ -1,11 +1,20 @@
 const transformationService = require('../service/transformation');
 
 const input = require('./data/inputs/transformationService.json');
-const joinedCourse = require('./data/outputs/joinedCourse.json');
 const denormalizedStudents = require('./data/outputs/denormalizedStudents.json');
+const joinedCourse = require('./data/outputs/joinedCourse.json');
+const joinedFollows = require('./data/outputs/joinedFollows.json');
 
 describe('Joins', () => {
   test('Join By Key', () => {
+    const { student_follow_subject: mainEntity, subjects: joinEntity } = input;
+
+    const result = transformationService.joinByKey(mainEntity, 'SubjectId', joinEntity, 'Id');
+
+    expect(result).toEqual(joinedFollows);
+  });
+
+  test('Join By Key With Missing Join Entry', () => {
     const { students: mainEntity, courses: joinEntity } = input;
 
     const result = transformationService.joinByKey(mainEntity, 'CourseId', joinEntity, 'Id');
