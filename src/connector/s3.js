006e081fe54e9c66cd96c184f12ef06700bbfadb
Fabio Romanini <fabioaromanini@gmail.com>
2019-11-25 11:32:07 -0200

    adds function for loading data into datawarehouse

--- src/connector/s3.js
+++ src/connector/s3.js
@@ -1,6 +1,6 @@
 const AWS = require('aws-sdk');
 
-const { SOURCE_NAME } = process.env;
+const { SOURCE_NAME, DATAWAREHOUSE_NAME } = process.env;
 
 const client = new AWS.S3();
 
@@ -14,4 +14,15 @@ async function extractEntity(entityName) {
   return { data: JSON.parse(response.Body.toString()), name: entityName };
 }
 
+function loadEntity(entityName, data) {
+  const params = {
+    Body: data,
+    Bucket: DATAWAREHOUSE_NAME,
+    Key: `a/${entityName}/${new Date()}.json`,
+  };
+
+  return client.putObject(params).promise();
+}
+
 exports.extractEntity = extractEntity;
+exports.loadEntity = loadEntity;
