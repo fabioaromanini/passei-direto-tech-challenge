2feba45bd9acffbeb84d511f31861b5156fc0892
Fabio Romanini <fabioaromanini@gmail.com>
2019-11-25 10:25:59 -0200

    adds all transformations to handler

--- src/handler.js
+++ src/handler.js
@@ -2,17 +2,16 @@ const sourceService = require('./service/source');
 const transformationService = require('./service/transformation');
 
 exports.extractTransform = async event => {
-  console.log('Hello, world with Circle-ci and deploys happening only on master:D');
   const data = await sourceService.extract();
-  const outputs = {
-    students: transformationService.denormalizeStudents(
-      data.students,
-      data.courses,
-      data.universities
-    ),
-    follows: transformationService.denormalizeFollows(data.student_follow_subject, data.subjects),
-  };
-
-  console.log(outputs.follows[0]);
-  console.log(outputs.students[0]);
+  const students = transformationService.denormalizeStudents(
+    data.students,
+    data.courses,
+    data.universities
+  );
+  const follows = transformationService.denormalizeFollows(
+    data.student_follow_subject,
+    data.subjects
+  );
+  const sessions = transformationService.parseSessions(data.sessions);
+  const subscriptions = transformationService.parseSubscriptions(data.subscriptions);
 };
