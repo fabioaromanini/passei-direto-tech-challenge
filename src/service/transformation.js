44db83efd81083b74639ee3dfdb097c664660193
Fabio Romanini <fabioaromanini@gmail.com>
2019-11-25 09:43:39 -0200

    removes parse code

--- src/service/transformation.js
+++ src/service/transformation.js
@@ -8,17 +8,6 @@ function indexEntityByKey(entity, keyName) {
   return entityByKey;
 }
 
-function parse(originalEntity, fieldNamesMapping) {
-  return originalEntity.map(originalEntry => {
-    const parsedEntry = {};
-    Object.keys(originalEntry).forEach(originalField => {
-      const newFieldName = fieldNamesMapping[originalField];
-      parsedEntry[newFieldName || originalField] = originalEntry[originalField];
-    });
-    return parsedEntry;
-  });
-}
-
 function joinByKey(mainEntity, mainEntityKey, joinEntity, joinEntityKey) {
   const joinEntityByKey = indexEntityByKey(joinEntity, joinEntityKey);
 
@@ -57,6 +46,5 @@ function denormalizeStudents(students, courses, universities) {
   });
 }
 
-exports.parse = parse;
 exports.joinByKey = joinByKey;
 exports.denormalizeStudents = denormalizeStudents;
--- src/test/transformation.test.js
+++ src/test/transformation.test.js
@@ -2,35 +2,15 @@ const transformationService = require('../service/transformation');
 
 const input = require('./data/inputs/transformationService.json');
 const joinedCourse = require('./data/outputs/joinedCourse.json');
-const parsedSubscription = require('./data/outputs/parsedSubscription.json');
 const denormalizedStudents = require('./data/outputs/denormalizedStudents.json');
 
 describe('Joins', () => {
-  beforeAll(() => {
-    const { courses, subscriptions } = input;
-
-    input.parsedCourse = transformationService.parse(courses, {
-      Name: 'CourseName',
-    });
-  });
-
   test('Join By Key', () => {
-    const { students: mainEntity, parsedCourse: joinEntity } = input;
+    const { students: mainEntity, courses: joinEntity } = input;
 
     const result = transformationService.joinByKey(mainEntity, 'CourseId', joinEntity, 'Id');
 
     expect(result).toEqual(joinedCourse);
-  });
-});
-
-describe('Parse', () => {
-  test('Parse field names', () => {
-    const result = transformationService.parse(input.subscriptions, {
-      PlanType: 'Type',
-      PaymentDate: 'Date',
-    });
-
-    expect(result).toEqual(parsedSubscription);
   });
 });
 
